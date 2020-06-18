a8e135df21c09b25080b8eec6e729ba56bb52f6a
Alex <format37@gmail.com>
2020-06-18 18:35:08 +0300

    new file:   calcubot.py

---
+++ calcubot.py
@@ -0,0 +1,23 @@
+import telebot
+
+def calcubot_init(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT):
+
+	SCRIPT_PATH     = '/home/format37_gmail_com/projects/calcubot_python/'
+
+	with open(SCRIPT_PATH+'token.key','r') as file:
+		API_TOKEN=file.read().replace('\n', '')
+		file.close()
+	calcubot = telebot.TeleBot(API_TOKEN)
+
+	WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+	WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
+
+	# Remove webhook, it fails sometimes the set if there is a previous webhook
+	calcubot.remove_webhook()
+
+	# Set webhook
+	wh_res = calcubot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,certificate=ope>
+	print('calcubot webhook set',wh_res)
+	print(WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
+
+	return calcubot
