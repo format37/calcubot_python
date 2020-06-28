23336c27f4cbbcf8454002fba23e9641944ca761
Alex <format37@gmail.com>
2020-06-28 18:59:59 +0300

    modified:   calcubot.py

--- calcubot.py
+++ calcubot.py
@@ -59,6 +59,7 @@ def calcubot_plot(SCRIPT_PATH,expression,god_mode,granted_words):
 		check_result	= check(expression,answer_max_lenght,god_mode,granted_words)
 		if check_result=='':
 			in_y = eval(secure_eval(SCRIPT_PATH,expression))
+			print(in_y)
 			fig = plt.figure()
 			for line_number in range(0,len(in_y)):
 				data_x = numpy.array( [i for i in range(0,len(in_y[line_number]))] )
