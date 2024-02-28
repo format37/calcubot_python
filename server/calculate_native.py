1ab736ef06d455d986e2e26b76346715733f542f
Alex <format37@gmail.com>
2024-02-29 01:20:40 +0400

    modified:   server/calculate_native.py

--- server/calculate_native.py
+++ server/calculate_native.py
@@ -16,7 +16,7 @@ import re
 
 try:
 	res_limits = resource.getrusage(resource.RUSAGE_SELF)
-	resource.setrlimit(resource.RLIMIT_CPU, (2, 2))
+	resource.setrlimit(resource.RLIMIT_CPU, (4, 4))
 	request = argv[1]
 	print( eval(request) )
 except Exception as e:
