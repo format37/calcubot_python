254fbad26abb8ce8b17593d7cb531d3784a32ab2
Alex <format37@gmail.com>
2024-05-30 16:42:06 +0400

    modified:   compose.sh

--- compose.sh
+++ compose.sh
@@ -1,2 +1,9 @@
-sudo docker-compose down -v
-sudo docker-compose up --build -d --force-recreate --remove-orphans
+# Create folder ./id_garden if not exists
+if [ ! -d "./id_garden" ]; then
+  mkdir ./id_garden
+fi
+# Remove all files in id_garden
+rm -rf ./id_garden/*
+# Down and up docker-compose
+sudo docker compose down -v
+sudo docker compose up --build --force-recreate --remove-orphans -d
