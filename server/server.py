4183908337bfdf8ead61467c89e371231959d58f
Alex <format37@gmail.com>
2024-05-31 14:34:24 +0400

    modified:   server/server.py

--- server/server.py
+++ server/server.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, Request, Header, Response, HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
-from fastapi.responses import JSONResponse # , FileResponse
+from fastapi.responses import JSONResponse
 import logging
 import subprocess
 import ast
 import telebot
-# from telebot.types import ReplyParameters
-# import os
 import json
 
 # Read unsecure words from file
@@ -27,68 +25,12 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.info('Logging started')
-# logging.config.fileConfig('logging.ini')
-# logger = logging.getLogger(__name__)
-
-# @app.exception_handler(HTTPException)
-# async def http_exception_handler(request: Request, exc: HTTPException):
-#     logger.warning(f"Invalid HTTP request received:")
-#     logger.warning(f"Method: {request.method}")
-#     logger.warning(f"URL: {request.url}")
-#     logger.warning(f"Headers: {request.headers}")
-#     logger.warning(f"Body: {await request.body()}")
-#     return JSONResponse(content={"status": "error"}, status_code=exc.status_code)
-
-
-# class RequestLoggingMiddleware(BaseHTTPMiddleware):
-#     async def dispatch(self, request: Request, call_next):
-#         logger.info(f"Incoming request: {request.method} {request.url}")
-#         logger.info(f"Headers: {request.headers}")
-#         logger.info(f"Body: {await request.body()}")
-#         response = await call_next(request)
-#         return response
-
-# app.add_middleware(RequestLoggingMiddleware)
 
 with open('config.json') as config_file:
-    # temp_config = json.load(config_file)
     bot = telebot.TeleBot(json.load(config_file)['TOKEN'])
 # drop config_file
 config_file.close()
-# bot_token = temp_config['TOKEN']
-# bot = telebot.TeleBot(bot_token)
 logger.info(f'Bot initialized: {bot}')
-
-# {'message_id': 11015390, 'from': {'id': 106129214, 'is_bot': False, 'first_name': 'Alex', 'username': 'format37', 'language_code': 'en', 'is_premium': True}, 'chat': {'id': 106129214, 'first_name': 'Alex', 'username': 'format37', 'type': 'private'}, 'date': 1716907673, 'text': '1+6'}
-
-# class From:
-#     def __init__(self, from_user):
-#         self.id = from_user['id']
-#         self.is_bot = from_user['is_bot']
-#         self.first_name = from_user['first_name']
-#         self.username = from_user['username']
-#         self.language_code = from_user['language_code']
-#         self.is_premium = from_user['is_premium']
-
-# class Chat:
-#     def __init__(self, chat):
-#         self.id = chat['id']
-#         self.first_name = chat['first_name']
-#         self.username = chat['username']
-#         self.type = chat['type']
-
-# class Message:
-#     def __init__(self, message):
-#         logger.info(f'### [Message]: {str(message)}')
-        
-#         self.message_id = message['message_id']        
-        
-#         self.from_user = From(message['from'])
-#         self.chat = Chat(message['chat'])
-
-#         self.text = message['text']
-#         self.date = message['date']
-#         self.type = 'message'
 
 
 async def is_complete_expression(expression):
@@ -129,10 +71,7 @@ async def secure_eval(expression, mode):
     
 @app.post("/message")
 async def call_message(request: Request, authorization: str = Header(None)):
-    # logger.info(f'call_message. bot: {str(bot)}')
     message = await request.json()
-    # logger.info(f'[call_message]: {str(message)}')
-
 #     response = """Hello,
 
 # This bot is currently undergoing maintenance related to migration to another server and refactoring. Please wait until June 3, 2023, for the bot to resume its normal functionality.
@@ -140,123 +79,50 @@ async def call_message(request: Request, authorization: str = Header(None)):
 
 # Warm regards,
 # Alex"""
-
 #     bot.send_message(message['chat']['id'], response)
 #     return Response(content='ok', status_code=200)
 
-    # return JSONResponse(content={
-    #         "type": "empty",
-    #         "body": ''
-    #     })    
-
     # Empty message
     if 'text' not in message:
-        # pass
-        # return JSONResponse(content={
-        #     "type": "empty",
-        #     "body": ''
-        # })    
         return Response(content='ok', status_code=200)
     expression = message['text']
     # Start or help
     if expression.startswith('/start') or expression.startswith('/help'):
-        # pass
         """link = 'https://rtlm.info/help.mp4'
         bot.send_video(message.chat.id, link,
                             reply_to_message_id=str(message))"""
-        # return JSONResponse(content={
-        #     "type": "text",
-        #     "body": 'This is a Python interpreter. Just type your expression and get the result. For example: 2+2'
-        # })
         # Send help message
         response = "This is a console calculator using Python syntax. Just type your expression and get the result. For example: 2+2"
         bot.send_message(message['chat']['id'], response)
         return Response(content='ok', status_code=200)
-    # start_from_cl = expression.startswith('/cl')
     # Not private chat
-    # if not start_from_cl and not message['chat']['type'] == 'private':
     if not message['chat']['type'] == 'private':
         # Exit from group
-        # logger.info(f'### ### ### Leaving group: {message["chat"]["id"]}')
         logger.info(f"### ### ### Leaving group: {message['chat']['id']}: {bot.leave_chat(message['chat']['id'])}")
         return Response(content='ok', status_code=200)
-        # return JSONResponse(content={
-        #     "type": "empty",
-        #     "body": ''
-        # })
     # Blocked user
     if await is_blocked_user(str(message['from']['id'])):
-        return JSONResponse(content={
-            "type": "empty",
-            "body": ''
-        })
-
-    # if start_from_cl:
-    #     expression = expression[4:]
-    #     if expression.strip() == '':
-    #         if message['chat']['type'] == 'private':
-    #             body = 'This is a Python interpreter. Just type your expression and get the result. For example: 2+2'
-    #         else:
-    #             body = 'This is a Python interpreter. Just type your expression and get the result. For example: /cl 2+2'
-    #         logger.info(f'[start_from_cl] User: {message["from"]["id"]} Request: {expression}')
-    #         pass
-    #         # return JSONResponse(content={
-    #         #     "type": "text",
-    #         #     "body": body
-    #         # })
-    #     else:
-    #         logger.info(f'[start_from_cl] User: {message["from"]["id"]} Request: {expression}')
-
-    
+        return Response(content='ok', status_code=200)
+   
     answer_max_lenght = 4095
-    # user_id = str(message['from']['id'])
     res = str(await secure_eval(expression, 'native'))[:answer_max_lenght]    
     response = f'{res} = {expression}'
-    # prefix = 'cl ' if start_from_cl else ''
-    # reply_to_message_id = message['message_id']
-    # logging.info(f'{prefix}User: {user_id} Request: {expression} Response: {response}, message_id: {message_id}')
-    
-    # message_instance = Message(message)
-    # bot.reply_to(message_instance, response)
-    # reply_parameters = ReplyParameters(message_id=reply_to_message_id)
-    # bot.send_message(
-    #     chat_id=message['chat']['id'],
-    #     text="This is a reply to your message.",
-    #     reply_parameters=reply_parameters
-    # )
     logging.info(f'Sending message to chat id: {message["chat"]["id"]}, response: {response}')
-    # logger.info(f'chat id type: {type(message["chat"]["id"])}')
-    # logger.info(f'response type: {type(response)}')
-    bot.send_message(message['chat']['id'], response)
-
-    # return JSONResponse(content={
-    #     "type": "text",
-    #     "body": response
-    # })
-    # Return empty
+    bot.send_message(message['chat']['id'], response)    
     
+    return Response(content='ok', status_code=200)
 
-    return JSONResponse(content={
-            "type": "empty",
-            "body": ''
-        })
 
-    
 # Post inline query
 @app.post("/inline")
 async def call_inline(request: Request, authorization: str = Header(None)):
     message = await request.json()
-    # from_user_id = inline_query.from_user.id
-    # inline_query_id = inline_query.id
     from_user_id = message['from_user_id']
     inline_query_id = message['inline_query_id']
     expression = message['query']
     # Blocked user
     if await is_blocked_user(from_user_id):
-        return JSONResponse(content={
-            "type": "inline",
-            "body": []
-        })
+        return JSONResponse(content={"status": "ok"})
     if not await is_complete_expression(expression):
         res = f'Incomplete expression: {expression}'
         answer = [res]
@@ -271,11 +137,6 @@ async def call_inline(request: Request, authorization: str = Header(None)):
     logger.info(f'User: {from_user_id} Inline request: {expression} Response: {res}')
 
     try:
-        # result_message = json.loads(result.text)
-        # answer = result_message['body']
-        # if result_message['type'] != 'inline':
-        #     logger.error(f'Inline: Invalid response type: {result_message["type"]}')
-        #     return JSONResponse(content={"status": "ok"})
         inline_elements = []
         for i in range(len(answer)):    
             element = telebot.types.InlineQueryResultArticle(
@@ -295,14 +156,5 @@ async def call_inline(request: Request, authorization: str = Header(None)):
     except Exception as e:
         logger.error(f'User: {from_user_id} Inline request: {expression}  Error processing inline query: {str(e)}')
 
-    # return JSONResponse(content={
-    #         "type": "inline",
-    #         "body": answer
-    #         })
-
-    # Return empty
-    return JSONResponse(content={
-            "type": "empty",
-            "body": ''
-        })
+    return JSONResponse(content={"status": "ok"})
 
