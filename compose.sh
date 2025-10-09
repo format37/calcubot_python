8d08685b5738724b4cbecb5ec7852549909916dd
Alex <format37@gmail.com>
2025-10-09 17:04:03 +0400

    separate instances for prod and test

--- compose.sh
+++ compose.sh
@@ -1,9 +1,9 @@
-# Create folder ./id_garden if not exists
-if [ ! -d "./id_garden" ]; then
-  mkdir ./id_garden
-fi
-# Remove all files in id_garden
-rm -rf ./id_garden/*
+# # Create folder ./id_garden if not exists
+# if [ ! -d "./id_garden" ]; then
+#   mkdir ./id_garden
+# fi
+# # Remove all files in id_garden
+# rm -rf ./id_garden/*
 # Down and up docker-compose
-sudo docker compose down -v
-sudo docker compose up --build --force-recreate --remove-orphans -d
+sudo docker compose down calcubot -v
+sudo docker compose up --build --force-recreate -d
